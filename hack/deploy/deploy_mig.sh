34fb6fefb29cdd28bf5e068acd6f5d9edff21e97
Jeff Ortel <jortel@redhat.com>
2019-05-21 12:41:53 -0500

    Consolidated MigMigration and MigStage. Adds MigMigration.staging. Deletes MigStage. Deletes velerorunner package.

--- hack/deploy/deploy_mig.sh
+++ hack/deploy/deploy_mig.sh
@@ -8,7 +8,6 @@ echo "==========================="
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/config/crds/migration_v1alpha1_migcluster.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/config/crds/migration_v1alpha1_migmigration.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/config/crds/migration_v1alpha1_migplan.yaml
-oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/config/crds/migration_v1alpha1_migstage.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/config/crds/migration_v1alpha1_migstorage.yaml
 
 # cluster-registry cluster CRD
--- pkg/controller/migmigration/backup.go
+++ pkg/controller/migmigration/backup.go
@@ -1,4 +1,4 @@
-package velerorunner
+package migmigration
 
 import (
 	"context"
