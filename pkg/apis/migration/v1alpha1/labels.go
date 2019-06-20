d13fef395361115920c915de8b5404d61630b008
Jeff Ortel <jortel@redhat.com>
2019-06-20 11:47:39 -0500

    Usability enhancements for correlation labels. (#168)

--- pkg/apis/migration/v1alpha1/labels.go
+++ pkg/apis/migration/v1alpha1/labels.go
@@ -8,21 +8,19 @@ import (
 
 // All known correlation labels.
 var KnownLabels = map[string]bool{
-	label(MigPlan{}):      true,
-	label(MigCluster{}):   true,
-	label(MigStorage{}):   true,
-	label(MigMigration{}): true,
+	labelKey(MigPlan{}):      true,
+	labelKey(MigCluster{}):   true,
+	labelKey(MigStorage{}):   true,
+	labelKey(MigMigration{}): true,
 }
 
-// Build  labels used to correlate CRs.
+// Build label (key, value) used to correlate CRs.
 // Format: <kind>: <uid>.  The <uid> should be the ObjectMeta.UID
-func buildCorrelationLabels(r interface{}, uid types.UID) map[string]string {
-	return map[string]string{
-		label(r): string(uid),
-	}
+func CorrelationLabel(r interface{}, uid types.UID) (key, value string) {
+	return labelKey(r), string(uid)
 }
 
 // Get a label (key) for the specified CR kind.
-func label(r interface{}) string {
+func labelKey(r interface{}) string {
 	return strings.ToLower(migref.ToKind(r))
 }
