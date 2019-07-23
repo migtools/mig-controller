925d9fdca875f7b4058addcf6c44f781015fb492
Jason Montleon <jmontleo@redhat.com>
2019-07-22 22:07:15 -0400

    fix velero deployment script

--- hack/deploy/deploy_velero.sh
+++ hack/deploy/deploy_velero.sh
@@ -4,10 +4,10 @@ echo
 echo "================"
 echo "Deploying Velero"
 echo "================"
+oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/00-mig-namespace.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/10-velero-crds.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/20-velero-deployment.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/30-restic-daemonset.yaml
-oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/40-mig-namespace.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/50-mig-sa.yaml
 oc apply -f https://raw.githubusercontent.com/fusor/mig-controller/master/hack/deploy/manifests/60-cloud-credentials.yaml
 
