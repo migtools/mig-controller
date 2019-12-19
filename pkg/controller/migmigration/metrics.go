a462d1f9de8823e3963ef2d00a451114591649fd
Derek Whatley <derekwhatley@gmail.com>
2019-12-19 13:23:20 -0500

    Adjust metric name to comply with metrics guidelines

--- pkg/controller/migmigration/metrics.go
+++ pkg/controller/migmigration/metrics.go
@@ -29,7 +29,7 @@ var (
 	// 'status' - [ idle, running, completed, error ]
 	// 'type'   - [ stage, final ]
 	migrationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "mig_migrations_count",
+		Name: "cam_app_workload_migration_total",
 		Help: "Count of MigMigrations sorted by status and type",
 	},
 		[]string{"type", "status"},
