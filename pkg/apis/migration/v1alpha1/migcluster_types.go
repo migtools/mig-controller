0db1908042a076154a9c00520bcd3d03406f01f1
Dylan Murray <dymurray@redhat.com>
2020-01-09 10:21:29 -0500

    Revert "migcluster: use caBundle unless 'insecureSkipTLSVerify' is true (#347)" (#396)
    This reverts commit 4edebc389b992773934e3a54c117366d319431a8.

--- pkg/apis/migration/v1alpha1/migcluster_types.go
+++ pkg/apis/migration/v1alpha1/migcluster_types.go
@@ -48,7 +48,6 @@ type MigClusterSpec struct {
 	CABundle                []byte                `json:"caBundle,omitempty"`
 	StorageClasses          []StorageClass        `json:"storageClasses,omitempty"`
 	AzureResourceGroup      string                `json:"azureResourceGroup,omitempty"`
-	InsecureSkipTLSVerify   bool                  `json:"insecureSkipTLSVerify"`
 }
 
 // MigClusterStatus defines the observed state of MigCluster
@@ -152,16 +151,12 @@ func (m *MigCluster) BuildRestConfig(c k8sclient.Client) (*rest.Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	var tlsClientConfig rest.TLSClientConfig
-	if m.Spec.InsecureSkipTLSVerify {
-		tlsClientConfig = rest.TLSClientConfig{Insecure: true}
-	} else {
-		tlsClientConfig = rest.TLSClientConfig{Insecure: false, CAData: m.Spec.CABundle}
-	}
 	restConfig := &rest.Config{
-		Host:            m.Spec.URL,
-		BearerToken:     string(secret.Data[SaToken]),
-		TLSClientConfig: tlsClientConfig,
+		Host:        m.Spec.URL,
+		BearerToken: string(secret.Data[SaToken]),
+		TLSClientConfig: rest.TLSClientConfig{
+			Insecure: true,
+		},
 	}
 
 	return restConfig, nil
