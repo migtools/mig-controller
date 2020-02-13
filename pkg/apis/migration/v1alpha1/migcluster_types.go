30bac50c599f33fdca1010cdc58cad2b40460e7b
Samuel Lucidi <slucidi@redhat.com>
2020-02-13 15:07:51 -0500

    migcluster: use the CABundle unless 'insecure' is true (#417)

--- pkg/apis/migration/v1alpha1/migcluster_types.go
+++ pkg/apis/migration/v1alpha1/migcluster_types.go
@@ -48,6 +48,7 @@ type MigClusterSpec struct {
 	CABundle                []byte                `json:"caBundle,omitempty"`
 	StorageClasses          []StorageClass        `json:"storageClasses,omitempty"`
 	AzureResourceGroup      string                `json:"azureResourceGroup,omitempty"`
+	Insecure                bool                  `json:"insecure"`
 }
 
 // MigClusterStatus defines the observed state of MigCluster
@@ -152,12 +153,16 @@ func (m *MigCluster) BuildRestConfig(c k8sclient.Client) (*rest.Config, error) {
 	if err != nil {
 		return nil, err
 	}
+	var tlsClientConfig rest.TLSClientConfig
+	if m.Spec.Insecure {
+		tlsClientConfig = rest.TLSClientConfig{Insecure: true}
+	} else {
+		tlsClientConfig = rest.TLSClientConfig{Insecure: false, CAData: m.Spec.CABundle}
+	}
 	restConfig := &rest.Config{
-		Host:        m.Spec.URL,
-		BearerToken: string(secret.Data[SaToken]),
-		TLSClientConfig: rest.TLSClientConfig{
-			Insecure: true,
-		},
+		Host:            m.Spec.URL,
+		BearerToken:     string(secret.Data[SaToken]),
+		TLSClientConfig: tlsClientConfig,
 	}
 
 	return restConfig, nil
