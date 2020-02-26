8787e6fa15010bbade81dcc1cecc11d0a4d1271e
Jeff Ortel <jortel@redhat.com>
2020-02-25 22:34:02 -0600

    Add Services, PVCs; add totals to Namespace. closes #414

---
+++ pkg/controller/discovery/container/pvc.go
@@ -0,0 +1,156 @@
+package container
+
+import (
+	"context"
+	"github.com/konveyor/mig-controller/pkg/controller/discovery/model"
+	"k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	"time"
+)
+
+// A collection of k8s PVC resources.
+type PvcCollection struct {
+	// Base
+	BaseCollection
+}
+
+func (r *PvcCollection) AddWatch(dsController controller.Controller) error {
+	err := dsController.Watch(
+		&source.Kind{
+			Type: &v1.PersistentVolumeClaim{},
+		},
+		&handler.EnqueueRequestForObject{},
+		r)
+	if err != nil {
+		Log.Trace(err)
+		return err
+	}
+
+	return nil
+}
+
+func (r *PvcCollection) Reconcile() error {
+	mark := time.Now()
+	sr := SimpleReconciler{
+		Db: r.ds.Container.Db,
+	}
+	err := sr.Reconcile(r)
+	if err != nil {
+		Log.Trace(err)
+		return err
+	}
+	r.hasReconciled = true
+	Log.Info(
+		"PVC Collection reconciled.",
+		"ns",
+		r.ds.Cluster.Namespace,
+		"name",
+		r.ds.Cluster.Name,
+		"duration",
+		time.Since(mark))
+
+	return nil
+}
+
+func (r *PvcCollection) GetDiscovered() ([]model.Model, error) {
+	models := []model.Model{}
+	onCluster := v1.PersistentVolumeClaimList{}
+	err := r.ds.Client.List(context.TODO(), nil, &onCluster)
+	if err != nil {
+		Log.Trace(err)
+		return nil, err
+	}
+	for _, discovered := range onCluster.Items {
+		pvc := &model.PVC{
+			Base: model.Base{
+				Cluster: r.ds.Cluster.PK,
+			},
+		}
+		pvc.With(&discovered)
+		models = append(models, pvc)
+	}
+
+	return models, nil
+}
+
+func (r *PvcCollection) GetStored() ([]model.Model, error) {
+	models := []model.Model{}
+	list, err := model.PVC{
+		Base: model.Base{
+			Cluster: r.ds.Cluster.PK,
+		},
+	}.List(
+		r.ds.Container.Db,
+		model.ListOptions{})
+	if err != nil {
+		Log.Trace(err)
+		return nil, err
+	}
+	for _, pvc := range list {
+		models = append(models, pvc)
+	}
+
+	return models, nil
+}
+
+//
+// Predicate methods.
+//
+
+func (r *PvcCollection) Create(e event.CreateEvent) bool {
+	Log.Reset()
+	object, cast := e.Object.(*v1.PersistentVolumeClaim)
+	if !cast {
+		return false
+	}
+	pvc := model.PVC{
+		Base: model.Base{
+			Cluster: r.ds.Cluster.PK,
+		},
+	}
+	pvc.With(object)
+	r.ds.Create(&pvc)
+
+	return false
+}
+
+func (r *PvcCollection) Update(e event.UpdateEvent) bool {
+	Log.Reset()
+	object, cast := e.ObjectNew.(*v1.PersistentVolumeClaim)
+	if !cast {
+		return false
+	}
+	pvc := model.PVC{
+		Base: model.Base{
+			Cluster: r.ds.Cluster.PK,
+		},
+	}
+	pvc.With(object)
+	r.ds.Update(&pvc)
+
+	return false
+}
+
+func (r *PvcCollection) Delete(e event.DeleteEvent) bool {
+	Log.Reset()
+	object, cast := e.Object.(*v1.PersistentVolumeClaim)
+	if !cast {
+		return false
+	}
+	pvc := model.PVC{
+		Base: model.Base{
+			Cluster: r.ds.Cluster.PK,
+		},
+	}
+	pvc.With(object)
+	r.ds.Delete(&pvc)
+
+	return false
+}
+
+func (r *PvcCollection) Generic(e event.GenericEvent) bool {
+	return false
+}
--- pkg/controller/discovery/model/pvc.go
+++ pkg/controller/discovery/model/pvc.go
@@ -6,14 +6,14 @@ import (
 )
 
 //
-// PV model.
-type PV struct {
+// PVC model.
+type PVC struct {
 	Base
 }
 
 //
-// Update the model `with` a k8s PersistentVolume.
-func (m *PV) With(object *v1.PersistentVolume) {
+// Update the model `with` a k8s PVC.
+func (m *PVC) With(object *v1.PersistentVolumeClaim) {
 	m.UID = string(object.UID)
 	m.Version = object.ResourceVersion
 	m.Namespace = object.Namespace
@@ -23,30 +23,30 @@ func (m *PV) With(object *v1.PersistentVolume) {
 
 //
 // Encode the object.
-func (m *PV) EncodeObject(pv *v1.PersistentVolume) {
-	object, _ := json.Marshal(pv)
+func (m *PVC) EncodeObject(pvc *v1.PersistentVolumeClaim) {
+	object, _ := json.Marshal(pvc)
 	m.Object = string(object)
 }
 
 //
 // Encode the object.
-func (m *PV) DecodeObject() *v1.PersistentVolume {
-	pv := &v1.PersistentVolume{}
-	json.Unmarshal([]byte(m.Object), pv)
-	return pv
+func (m *PVC) DecodeObject() *v1.PersistentVolumeClaim {
+	pvc := &v1.PersistentVolumeClaim{}
+	json.Unmarshal([]byte(m.Object), pvc)
+	return pvc
 }
 
 //
 // Fetch the from in the DB.
-func (m PV) List(db DB, options ListOptions) ([]*PV, error) {
-	list := []*PV{}
+func (m PVC) List(db DB, options ListOptions) ([]*PVC, error) {
+	list := []*PVC{}
 	listed, err := Table{db}.List(&m, options)
 	if err != nil {
 		Log.Trace(err)
 		return nil, err
 	}
 	for _, intPtr := range listed {
-		list = append(list, intPtr.(*PV))
+		list = append(list, intPtr.(*PVC))
 	}
 
 	return list, nil
@@ -54,27 +54,27 @@ func (m PV) List(db DB, options ListOptions) ([]*PV, error) {
 
 //
 // Fetch the model from the DB.
-func (m *PV) Get(db DB) error {
+func (m *PVC) Get(db DB) error {
 	return Table{db}.Get(m)
 }
 
 //
 // Insert the model into the DB.
-func (m *PV) Insert(db DB) error {
+func (m *PVC) Insert(db DB) error {
 	m.SetPk()
 	return Table{db}.Insert(m)
 }
 
 //
 // Update the model in the DB.
-func (m *PV) Update(db DB) error {
+func (m *PVC) Update(db DB) error {
 	m.SetPk()
 	return Table{db}.Update(m)
 }
 
 //
 // Delete the model in the DB.
-func (m *PV) Delete(db DB) error {
+func (m *PVC) Delete(db DB) error {
 	m.SetPk()
 	return Table{db}.Delete(m)
 }
