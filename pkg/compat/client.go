0cdf840402609bc02976ca8466b1886695f4724a
Danil-Grigorev <dgrigore@redhat.com>
2020-03-20 01:07:27 +0100

    Added up and down conversion functions

--- pkg/compat/client.go
+++ pkg/compat/client.go
@@ -67,45 +67,44 @@ func NewClient(restCfg *rest.Config) (k8sclient.Client, error) {
 }
 
 //
-// Down convert a resource as needed based on cluster version
-func (c Client) downConvert(obj runtime.Object) runtime.Object {
+// supportedVersion will determine correct version of the object provided, based on cluster version
+func (c Client) supportedVersion(obj runtime.Object) runtime.Object {
 	if c.Minor < 16 {
+		switch obj.(type) {
+
 		// Deployment
-		if _, cast := obj.(*appsv1.Deployment); cast {
+		case *appsv1.Deployment:
 			return &appsv1beta1.Deployment{}
-		}
-		if _, cast := obj.(*appsv1.DeploymentList); cast {
+		case *appsv1.DeploymentList:
 			return &appsv1beta1.DeploymentList{}
-		}
+
 		// DaemonSet
-		if _, cast := obj.(*appsv1.DaemonSet); cast {
+		case *appsv1.DaemonSet:
 			return &extv1beta1.DaemonSet{}
-		}
-		if _, cast := obj.(*appsv1.DaemonSetList); cast {
+		case *appsv1.DaemonSetList:
 			return &extv1beta1.DaemonSetList{}
-		}
+
 		// ReplicaSet
-		if _, cast := obj.(*appsv1.ReplicaSet); cast {
+		case *appsv1.ReplicaSet:
 			return &extv1beta1.ReplicaSet{}
-		}
-		if _, cast := obj.(*appsv1.ReplicaSetList); cast {
+		case *appsv1.ReplicaSetList:
 			return &extv1beta1.ReplicaSetList{}
-		}
+
 		// StatefulSet
-		if _, cast := obj.(*appsv1.StatefulSet); cast {
+		case *appsv1.StatefulSet:
 			return &appsv1beta1.StatefulSet{}
-		}
-		if _, cast := obj.(*appsv1.StatefulSetList); cast {
+		case *appsv1.StatefulSetList:
 			return &appsv1beta1.StatefulSetList{}
 		}
 	}
 
 	if c.Minor < 8 {
+		switch obj.(type) {
+
 		// CronJob
-		if _, cast := obj.(*batchv1beta.CronJobList); cast {
+		case *batchv1beta.CronJobList:
 			return &batchv2alpha.CronJobList{}
-		}
-		if _, cast := obj.(*batchv1beta.CronJob); cast {
+		case *batchv1beta.CronJob:
 			return &batchv2alpha.CronJob{}
 		}
 	}
@@ -114,69 +113,85 @@ func (c Client) downConvert(obj runtime.Object) runtime.Object {
 }
 
 //
+// Down convert a resource as needed based on cluster version.
+func (c Client) downConvert(ctx context.Context, obj runtime.Object) (runtime.Object, error) {
+	new := c.supportedVersion(obj)
+	if new == obj {
+		return obj, nil
+	}
+
+	err := scheme.Scheme.Convert(obj, new, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return new, nil
+}
+
+//
+// upConvert will convert src resource to dst as needed based on cluster version
+func (c Client) upConvert(ctx context.Context, src runtime.Object, dst runtime.Object) error {
+	if c.supportedVersion(dst) == dst {
+		dst = src
+		return nil
+	}
+
+	return scheme.Scheme.Convert(src, dst, ctx)
+}
+
+//
 // Get the specified resource.
 // The resource will be converted to a compatible version as needed.
 func (c Client) Get(ctx context.Context, key k8sclient.ObjectKey, in runtime.Object) error {
-	obj := c.downConvert(in)
+	obj := c.supportedVersion(in)
 	err := c.Client.Get(ctx, key, obj)
 	if err != nil {
 		return err
 	}
-	err = scheme.Scheme.Convert(obj, in, ctx)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return c.upConvert(ctx, obj, in)
 }
 
 //
 // List the specified resource.
 // The resource will be converted to a compatible version as needed.
 func (c Client) List(ctx context.Context, opt *k8sclient.ListOptions, in runtime.Object) error {
-	obj := c.downConvert(in)
-	err := c.Client.List(ctx, opt, obj)
+	obj, err := c.downConvert(ctx, in)
 	if err != nil {
 		return err
 	}
-	err = scheme.Scheme.Convert(obj, in, ctx)
+	err = c.Client.List(ctx, opt, obj)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return c.upConvert(ctx, obj, in)
 }
 
-//
-// List the specified resource.
+// Create the specified resource.
 // The resource will be converted to a compatible version as needed.
 func (c Client) Create(ctx context.Context, in runtime.Object) error {
-	obj := c.downConvert(in)
-	err := scheme.Scheme.Convert(in, obj, ctx)
+	obj, err := c.downConvert(ctx, in)
 	if err != nil {
 		return err
 	}
 	return c.Client.Create(ctx, obj)
 }
 
-//
 // Delete the specified resource.
 // The resource will be converted to a compatible version as needed.
 func (c Client) Delete(ctx context.Context, in runtime.Object, opt ...k8sclient.DeleteOptionFunc) error {
-	obj := c.downConvert(in)
-	err := scheme.Scheme.Convert(in, obj, ctx)
+	obj, err := c.downConvert(ctx, in)
 	if err != nil {
 		return err
 	}
 	return c.Client.Delete(ctx, obj, opt...)
 }
 
-//
 // Update the specified resource.
 // The resource will be converted to a compatible version as needed.
 func (c Client) Update(ctx context.Context, in runtime.Object) error {
-	obj := c.downConvert(in)
-	err := scheme.Scheme.Convert(in, obj, ctx)
+	obj, err := c.downConvert(ctx, in)
 	if err != nil {
 		return err
 	}
