8658ae14601ff160a3fbfe4467df785820e96d6c
Dylan Murray <dymurray@redhat.com>
2020-04-03 11:30:03 -0400

    Construct default transport

--- pkg/cloudprovider/aws.go
+++ pkg/cloudprovider/aws.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -365,17 +366,22 @@ func (r *S3Test) Run() error {
 }
 
 func (r *S3Test) newSession() (*session.Session, error) {
+	// copied from net/http
 	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: r.insecure},
-	}
-
-	var hasHttpProxy, hasHttpsProxy, hasNoProxy bool
-	hasHttpProxy, _ = Settings.HasProxyVar(settings.HttpProxy)
-	hasHttpsProxy, _ = Settings.HasProxyVar(settings.HttpsProxy)
-	hasNoProxy, _ = Settings.HasProxyVar(settings.NoProxy)
-
-	if hasHttpProxy || hasHttpsProxy || hasNoProxy {
-		transport.Proxy = http.ProxyFromEnvironment
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: r.insecure,
+		},
 	}
 
 	client := &http.Client{Transport: transport}
