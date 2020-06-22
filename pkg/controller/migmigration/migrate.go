7e193765e0e7c60935851e16946c55c57127d83f
Dylan Murray <dymurray@redhat.com>
2020-06-22 19:12:54 -0400

    HF5 Remove CSVs from backup

--- pkg/controller/migmigration/migrate.go
+++ pkg/controller/migmigration/migrate.go
@@ -152,7 +152,7 @@ func (r *ReconcileMigMigration) getBackupResources(migration *migapi.MigMigratio
 // Get the resources to be excluded from backup
 func (r *ReconcileMigMigration) getExcludedResources(migration *migapi.MigMigration) []string {
 	if Settings.Migration.DisableImageMigration {
-		return []string{"imagestreams", "imagestreamtags", "templateinstances"}
+		return []string{"imagestreams", "imagestreamtags", "templateinstances", "clusterserviceversions"}
 	}
 	return []string{"templateinstances"}
 }
