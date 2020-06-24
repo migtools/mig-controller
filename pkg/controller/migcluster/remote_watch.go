0023b3f6ab46e7860e0772dc730786827fa1bdd4
Dylan Murray <dymurray@redhat.com>
2020-06-24 06:18:53 -0400

    HF5 Remove CSVs from backup (#576)
    * HF5 Remove CSVs from backup
    
    * Update log message
    
    * Add subscriptions to excluded list

--- pkg/controller/migcluster/remote_watch.go
+++ pkg/controller/migcluster/remote_watch.go
@@ -63,10 +63,11 @@ func StartRemoteWatch(r *ReconcileMigCluster, config remote.ManagerConfig) error
 
 	sigStopChan := make(chan struct{})
 	log.Info("[rWatch] Starting manager")
-	log.Info("[HF] HotFix #4")
+	log.Info("[HF] HotFix #5")
 	log.Info("[HF] HF#2: Added imagestreamtags to backup exclusion list")
 	log.Info("[HF] HF#3: Added templateinstances to backup exclusion list, prevents templateinstance set username privilege error")
 	log.Info("[HF] HF#4: Requeue rather than fail if error is resource conflict")
+	log.Info("[HF] HF#5: Added OLM resources to backup exclusion list")
 	go mgr.Start(sigStopChan)
 
 	log.Info("[rWatch] Manager started")
