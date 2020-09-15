dd58cce330fd9f6a6d691e9836a2920338147f00
Derek Whatley <derekwhatley@gmail.com>
2020-09-15 19:54:50 -0400

    Add AGE to printer columns

--- pkg/apis/migration/v1alpha1/migcluster_types.go
+++ pkg/apis/migration/v1alpha1/migcluster_types.go
@@ -79,6 +79,7 @@ type MigClusterStatus struct {
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="URL",type=string,JSONPath=".spec.url"
 // +kubebuilder:printcolumn:name="Host",type=boolean,JSONPath=".spec.isHostCluster"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type MigCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
