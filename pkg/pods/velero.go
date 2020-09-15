8aabb09538d3e2e14e8c0f03e3d581a84b8e9a8f
Scott Seago <sseago@redhat.com>
2020-09-15 11:08:32 -0400

    Filter velero pod list on currently-running (non-terminating) pods

---
+++ pkg/pods/velero.go
@@ -0,0 +1,43 @@
+package pods
+
+import (
+	"context"
+
+	liberr "github.com/konveyor/controller/pkg/error"
+	migapi "github.com/konveyor/mig-controller/pkg/apis/migration/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Find all velero pods for the specified client.
+func FindVeleroPods(client k8sclient.Client) ([]corev1.Pod, error) {
+	var podList []corev1.Pod
+	list := &corev1.PodList{}
+	labelSelector := labels.SelectorFromSet(
+		map[string]string{
+			"component": "velero",
+		})
+	fieldSelector := fields.SelectorFromSet(
+		map[string]string{
+			"status.phase": "Running",
+		})
+	err := client.List(
+		context.TODO(),
+		&k8sclient.ListOptions{
+			Namespace:     migapi.VeleroNamespace,
+			LabelSelector: labelSelector,
+			FieldSelector: fieldSelector,
+		},
+		list)
+	if err != nil {
+		return nil, liberr.Wrap(err)
+	}
+	for _, pod := range list.Items {
+		if pod.DeletionTimestamp == nil {
+			podList = append(podList, pod)
+		}
+	}
+	return podList, nil
+}
