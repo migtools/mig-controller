a7137a8631101eea1c133ad6c7aeb635b8171cf6
Pranav Gaikwad <pgaikwad@redhat.com>
2020-09-28 10:15:51 -0400

    remove constants, change named return

--- pkg/controller/migmigration/backup.go
+++ pkg/controller/migmigration/backup.go
@@ -122,9 +122,6 @@ func (t *Task) getStageBackup() (*velero.Backup, error) {
 }
 
 func (t *Task) getPodVolumeBackupsForBackup(backup *velero.Backup) *velero.PodVolumeBackupList {
-<<<<<<< HEAD
-	return nil
-=======
 	nl := map[string]string{
 		velero.BackupNameLabel: backup.Name,
 	}
@@ -144,7 +141,6 @@ func (t *Task) getPodVolumeBackupsForBackup(backup *velero.Backup) *velero.PodVo
 		return nil
 	}
 	return &list
->>>>>>> 6234c232... PR changes
 }
 
 // Get an existing Backup on the source cluster.
@@ -168,38 +164,6 @@ func (t Task) getBackup(labels map[string]string) (*velero.Backup, error) {
 	return nil, nil
 }
 
-<<<<<<< HEAD
-func (t *Task) getPodVolumeBackup(backup *velero.Backup) {
-	nl := map[string]string{
-		"velero.io/backup-name": backup.Name,
-	}
-
-	client, err := t.getSourceClient()
-	if err != nil {
-		log.Trace(err)
-	}
-	list := velero.PodVolumeBackupList{}
-	err = client.List(
-		context.TODO(),
-		k8sclient.MatchingLabels(nl),
-		&list)
-	if err != nil {
-		log.Trace(err)
-	}
-	if len(list.Items) > 0 {
-		log.Info(fmt.Sprintf("total %v", len(list.Items)))
-		for _, podVolumeBackup := range list.Items {
-			t.Progress = append(t.Progress, fmt.Sprintf("[VolumeBackup %v] %v out of %v bytes backed up", podVolumeBackup.Name, podVolumeBackup.Status.Progress.BytesDone, podVolumeBackup.Status.Progress.TotalBytes))
-			log.Info(fmt.Sprintf("Remaining: %v", podVolumeBackup.Name))
-			log.Info(fmt.Sprintf("Remaining: %v", podVolumeBackup.Status.Progress.BytesDone))
-			log.Info(fmt.Sprintf("Total: %v", podVolumeBackup.Status.Progress.TotalBytes))
-		}
-
-	}
-}
-
-=======
->>>>>>> 6234c232... PR changes
 // Update Task.Progress with latest available progress information
 func (t *Task) updateBackupProgress(backup *velero.Backup, pvbList *velero.PodVolumeBackupList) {
 	progress := []string{}
@@ -215,9 +179,6 @@ func (t *Task) updateBackupProgress(backup *velero.Backup, pvbList *velero.PodVo
 	}
 	if pvbList != nil {
 		for _, pvb := range pvbList.Items {
-<<<<<<< HEAD
-			// gather progress here
-=======
 			progress = append(progress,
 				fmt.Sprintf(
 					"PodVolumeBackup %s/%s: %d of %d bytes backed up",
@@ -225,7 +186,6 @@ func (t *Task) updateBackupProgress(backup *velero.Backup, pvbList *velero.PodVo
 					pvb.Name,
 					pvb.Status.Progress.BytesDone,
 					pvb.Status.Progress.TotalBytes))
->>>>>>> 6234c232... PR changes
 		}
 	}
 	t.Progress = progress
