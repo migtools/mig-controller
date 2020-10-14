2a7150b490ac442d0e32f6109dfe2c320107b542
Alay Patel <alay1431@gmail.com>
2020-10-14 08:39:39 -0400

    fixups 3:
    - refactor error messages in health checks
    - fixup comments

--- pkg/controller/migplan/registry.go
+++ pkg/controller/migplan/registry.go
@@ -215,7 +215,7 @@ func (r ReconcileMigPlan) ensureRegistryDeployment(client k8sclient.Client, plan
 		return liberr.Wrap(err)
 	}
 
-	//Construct Registry DC
+	// Construct Registry DC
 	newDeployment := plan.BuildRegistryDeployment(storage, proxySecret, name, dirName, registryImage)
 	foundDeployment, err := plan.GetRegistryDeployment(client)
 	if err != nil {
