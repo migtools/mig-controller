9b43c97cc04237fb9ca9237bb43a2c5adff530a4
Jaydipkumar Arvindbhai Gabani <gabanijaydip@gmail.com>
2020-11-05 14:25:41 -0500

    [Mig-119] Hooks stage progress (#739)
    * initial stage pod progress
    
    * Progress reporting for Hooks
    
    * update hooks progress message
    
    * remove restic pod progress messages
    
    * Update pkg/controller/migmigration/stage.go
    
    Co-authored-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    * Update pkg/controller/migmigration/stage.go
    
    Co-authored-by: Pranav Gaikwad <pgaikwad@redhat.com>
    
    Co-authored-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- pkg/controller/migmigration/hooks.go
+++ pkg/controller/migmigration/hooks.go
@@ -86,13 +86,21 @@ func (t *Task) ensureJob(job *batchv1.Job, hook migapi.MigPlanHook, migHook miga
 		return false, err
 	} else if runningJob.Status.Failed >= HookJobFailedLimit {
 		err := fmt.Errorf("Hook job %s failed.", runningJob.Name)
+		t.setProgress([]string{
+			fmt.Sprintf("Job %s/%s: Failed", runningJob.Namespace, runningJob.Name)})
 		return false, err
 	} else if len(runningJob.Status.Conditions) > 0 && runningJob.Status.Conditions[0].Reason == BackoffLimitExceededError {
 		err := fmt.Errorf("Hook job %s failed.", runningJob.Name)
+		t.setProgress([]string{
+			fmt.Sprintf("Job %s/%s: Failed", runningJob.Namespace, runningJob.Name)})
 		return false, err
 	} else if runningJob.Status.Succeeded == 1 {
+		t.setProgress([]string{
+			fmt.Sprintf("Job %s/%s: Succeeded", runningJob.Namespace, runningJob.Name)})
 		return true, nil
 	} else {
+		t.setProgress([]string{
+			fmt.Sprintf("Job %s/%s: Running", runningJob.Namespace, runningJob.Name)})
 		return false, nil
 	}
 }
