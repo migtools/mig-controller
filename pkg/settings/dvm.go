b89654d04b69fa6a7f397aeb54490b7170b7e212
Pranav Gaikwad <pgaikwad@redhat.com>
2021-01-06 11:08:33 -0500

    allow reading Rsync options through environment variables (#874)

---
+++ pkg/settings/dvm.go
@@ -0,0 +1,56 @@
+package settings
+
+import (
+	"os"
+	"strings"
+)
+
+// Rsync options
+const (
+	RsyncOptBwLimit   = "RSYNC_OPT_BWLIMIT"
+	RsyncOptPartial   = "RSYNC_OPT_PARTIAL"
+	RsyncOptArchive   = "RSYNC_OPT_ARCHIVE"
+	RsyncOptDelete    = "RSYNC_OPT_DELETE"
+	RsyncOptHardLinks = "RSYNC_OPT_HARDLINKS"
+	RsyncOptInfo      = "RSYNC_OPT_INFO"
+	RsyncOptExtras    = "RSYNC_OPT_EXTRAS"
+)
+
+// RsyncOpts Rsync Options
+//	BwLimit: equivalent to --bwlimit=<integer>
+//	Archive: whether to set --archive option or not
+//	Partial: whether to set --partial option or not
+//	Delete:  whether to set --delete option or not
+//	HardLinks: whether to set --hard-links option or not
+//	Extras: arbitrary rsync options provided by the user
+type RsyncOpts struct {
+	BwLimit   int
+	Archive   bool
+	Partial   bool
+	Delete    bool
+	HardLinks bool
+	Info      string
+	Extras    []string
+}
+
+// Load load rsync options
+func (r *RsyncOpts) Load() error {
+	var err error
+	r.BwLimit, err = getEnvLimit(RsyncOptBwLimit, -1)
+	if err != nil {
+		return err
+	}
+	r.Archive = getEnvBool(RsyncOptArchive, true)
+	r.Partial = getEnvBool(RsyncOptPartial, true)
+	r.Delete = getEnvBool(RsyncOptDelete, true)
+	r.HardLinks = getEnvBool(RsyncOptHardLinks, true)
+	infoOpts := os.Getenv(RsyncOptInfo)
+	if len(infoOpts) > 0 {
+		r.Info = infoOpts
+	}
+	rsyncExtraOpts := os.Getenv(RsyncOptExtras)
+	if len(rsyncExtraOpts) > 0 {
+		r.Extras = strings.Fields(rsyncExtraOpts)
+	}
+	return err
+}
