9b9135c9495ac382c8f4cb94c3d1247f1dd3293b
Alay Patel <alay1431@gmail.com>
2021-07-01 10:15:52 -0400

    fix pod manager bug for verify, wrap errors

--- pkg/health/pods.go
+++ pkg/health/pods.go
@@ -3,6 +3,8 @@ package health
 import (
 	"context"
 	"fmt"
+	liberr "github.com/konveyor/controller/pkg/error"
+	"k8s.io/apimachinery/pkg/api/meta"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -21,24 +23,29 @@ const (
 
 var podManagingResources = [...]schema.GroupVersionKind{
 	schema.GroupVersionKind{
-		Group: "apps",
-		Kind:  "statefulset",
+		Group:   "apps",
+		Kind:    "statefulset",
+		Version: "v1",
 	},
 	schema.GroupVersionKind{
-		Group: "apps",
-		Kind:  "deployment",
+		Group:   "apps",
+		Kind:    "deployment",
+		Version: "v1",
 	},
 	schema.GroupVersionKind{
-		Group: "",
-		Kind:  "replicationcontroller",
+		Group:   "",
+		Kind:    "replicationcontroller",
+		Version: "v1",
 	},
 	schema.GroupVersionKind{
-		Group: "extensions",
-		Kind:  "replicaset",
+		Group:   "apps",
+		Kind:    "replicaset",
+		Version: "v1",
 	},
 	schema.GroupVersionKind{
-		Group: "apps.openshift.io",
-		Kind:  "deploymentconfig",
+		Group:   "apps.openshift.io",
+		Kind:    "deploymentconfig",
+		Version: "v1",
 	},
 }
 
@@ -51,22 +58,25 @@ func PodManagersRecreated(client k8sclient.Client, options *k8sclient.ListOption
 		podManagersList.SetGroupVersionKind(gvk)
 		err := client.List(context.TODO(), &podManagersList, options)
 		if err != nil {
-			return false, err
+			if meta.IsNoMatchError(err) {
+				continue
+			}
+			return false, liberr.Wrap(err)
 		}
 
 		for _, podManager := range podManagersList.Items {
 			expectedReplicas, found, err := unstructured.NestedFieldNoCopy(podManager.Object, "spec", "replicas")
 			if err != nil {
-				return false, err
+				return false, liberr.Wrap(err)
 			}
 			if !found {
 				err = fmt.Errorf("Replicas field was not found in %s kind", podManager.GetKind())
-				return false, err
+				return false, liberr.Wrap(err)
 			}
 			replicas, ok := expectedReplicas.(int64)
 			if !ok {
 				err = fmt.Errorf("Can't convert Replicas field to int64 for %s kind", podManager.GetKind())
-				return false, err
+				return false, liberr.Wrap(err)
 			}
 			if replicas == 0 {
 				continue
@@ -74,7 +84,7 @@ func PodManagersRecreated(client k8sclient.Client, options *k8sclient.ListOption
 
 			readyReplicas, foundReady, err := unstructured.NestedFieldNoCopy(podManager.Object, "status", "readyReplicas")
 			if err != nil {
-				return false, err
+				return false, liberr.Wrap(err)
 			}
 
 			if !foundReady || expectedReplicas != readyReplicas {
@@ -93,7 +103,7 @@ func DaemonSetsRecreated(client k8sclient.Client, options *k8sclient.ListOptions
 
 	err := client.List(context.TODO(), &daemonSetList, options)
 	if err != nil {
-		return false, err
+		return false, liberr.Wrap(err)
 	}
 
 	for _, ds := range daemonSetList.Items {
