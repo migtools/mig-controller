894a01e1689029df37f70b180d60ab21e13c4dc7
Pranav Gaikwad <pgaikwad@redhat.com>
2021-11-19 09:52:24 -0500

    Update predicates to filter events outside of migration namespace (#1226)
    * update predicates to filter events outside of migration namespace
    
    * update tests to create CRs in migration ns

--- pkg/controller/directimagestreammigration/directimagestreammigration_controller_test.go
+++ pkg/controller/directimagestreammigration/directimagestreammigration_controller_test.go
@@ -23,6 +23,7 @@ import (
 	migrationv1alpha1 "github.com/konveyor/mig-controller/pkg/apis/migration/v1alpha1"
 	"github.com/onsi/gomega"
 	"golang.org/x/net/context"
+	kapi "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -33,13 +34,13 @@ import (
 
 var c client.Client
 
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
+var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: migrationv1alpha1.OpenshiftMigrationNamespace}}
 
 const timeout = time.Second * 5
 
 func TestReconcile(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
-	instance := &migrationv1alpha1.DirectImageStreamMigration{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+	instance := &migrationv1alpha1.DirectImageStreamMigration{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: migrationv1alpha1.OpenshiftMigrationNamespace}}
 
 	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
 	// channel when it is finished.
@@ -53,7 +54,15 @@ func TestReconcile(t *testing.T) {
 	stopFunc := StartTestManager(mgr, g)
 
 	defer stopFunc()
-
+	err = c.Create(context.TODO(), &kapi.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: migrationv1alpha1.OpenshiftMigrationNamespace,
+		},
+	})
+	if apierrors.IsInvalid(err) {
+		t.Logf("failed to create object, got an invalid object error: %v", err)
+		return
+	}
 	// Create the DirectImageStreamMigration object and expect the Reconcile and Deployment to be created
 	err = c.Create(context.TODO(), instance)
 	// The instance object may not be a valid object because it might be missing some required fields.
