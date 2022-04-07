a929017a0ef2a5f72cdc32ec8d88589366bb1232
Pranav Gaikwad <pgaikwad@redhat.com>
2022-04-07 11:25:58 -0400

    fix checksum option in Rsync command (#1268) (#1274)

--- pkg/controller/directvolumemigration/rsync.go
+++ pkg/controller/directvolumemigration/rsync.go
@@ -698,7 +698,7 @@ func (t *Task) getNamespacedPVCPairs() (map[string][]transfer.PVCPair, error) {
 }
 
 func (t *Task) getSourceSecurityGroupInfo(srcClient compat.Client, pvcPairMap map[string][]transfer.PVCPair) (pvcWithSecurityContextInfo, error) {
-	var pvcInfo pvcWithSecurityContextInfo
+	pvcInfo := make(pvcWithSecurityContextInfo)
 
 	for bothNS := range pvcPairMap {
 		srcNs := getSourceNs(bothNS)
@@ -737,8 +737,8 @@ func (t *Task) getSourceSecurityGroupInfo(srcClient compat.Client, pvcPairMap ma
 				seLinuxOptions:     nil,
 				verify:             pvc.Verify,
 			}
-			pvcInfo.Add(pvc.Name, pvc.Namespace, secInfo)
 		}
+		pvcInfo.Add(pvc.Name, pvc.Namespace, secInfo)
 	}
 	return pvcInfo, nil
 }
