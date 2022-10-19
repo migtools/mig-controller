edb1782cd7c14b89fe37e49bfbe384a3672e985b
Pranav Gaikwad <pgaikwad@redhat.com>
2022-10-19 13:57:41 -0400

    copy labels from source namespaces (#1319)

--- pkg/controller/directimagemigration/namespace.go
+++ pkg/controller/directimagemigration/namespace.go
@@ -63,6 +63,7 @@ func (t *Task) ensureDestinationNamespaces() error {
 			ObjectMeta: metav1.ObjectMeta{
 				Name:        destNsName,
 				Annotations: srcNS.Annotations,
+				Labels:      srcNS.Labels,
 			},
 		}
 		existingNamespace := &corev1.Namespace{}
